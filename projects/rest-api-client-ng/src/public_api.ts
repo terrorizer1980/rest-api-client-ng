306c6efcbaed4448b2e201f04cd74060b2eae973
Ara Winters <ara.winters@gmail.com>
2019-01-10 09:50:04 -0800

    removed old boilerplate

--- projects/rest-api-client-ng/src/public_api.ts
+++ projects/rest-api-client-ng/src/public_api.ts
@@ -2,9 +2,6 @@
  * Public API Surface of rest-api-client-ng
  */
 
-//export * from './lib/rest-api-client-ng.service';
-//export * from './lib/rest-api-client-ng.module';
-
 /** Swagger auto-generated */
 export * from './lib/api/api';
 export * from './lib/model/models';
